e768c20ab5ec571d7e005da9e6cac1c3a94110e9
Joe Ste.Marie <stemarie.joe@gmail.com>
2014-11-06 01:02:14 -0500

    Moved Mergesort to function

--- MergeSort.py
+++ MergeSort.py
@@ -2,46 +2,35 @@ def split(list):
     half = len(list)//2
     return list[:half], list[half:]
 
-array = [5,4,1,8,7,2,6,3]
-A, B = split(array)
-n=len(array)
+def mergesort(array):
+    A, B = split(array)
+    n=len(array)
 
-A = sorted(A)
-B = sorted(B)
+    A = sorted(A)
+    B = sorted(B)
 
-print(A)
-print(B)
+    array_sorted=[]
 
-array_sorted=[]
+    i=0
+    j=0
 
-i=0
-j=0
+    for k in range(0,n):
+        if i>=len(A) and j>=len(B):
+            k=n
+        if i>=len(A):
+            array_sorted.append(B[j])
+            j+=1
+        elif j>=len(B):
+            array_sorted.append(A[i])
+            i+=1
+        elif A[i] < B[j]:
+            array_sorted.append(A[i])
+            i+=1
+        elif A[i] > B[j]:
+            array_sorted.append(B[j])
+            j+=1
 
-for k in range(0,n):
-    if i>=len(A):
-        array_sorted.append(B[j])
-        j+=1
-        k+=1
-        print(k)
-        print(array_sorted)
-    if j>=len(B):
-        array_sorted.append(A[i])
-        i+=1
-        k+=1
-        print(k)
-        print(array_sorted)
-    elif A[i] < B[j]:
-        array_sorted.append(A[i])
-        i+=1
-        k+=1
-        print(k)
-        print(array_sorted)
-    elif A[i] > B[j]:
-        array_sorted.append(B[j])
-        j+=1
-        k+=1
-        print(k)
-        print(array_sorted)
-    else:
-        print("It's done")
-        print(array_sorted)
+    return array_sorted
+
+list = [88,99,77,150,143,200]
+print(mergesort(list))
